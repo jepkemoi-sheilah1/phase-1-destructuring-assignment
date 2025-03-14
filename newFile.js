a85cacd3a6b20c555001250eaba69e499cf61d93
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-14 12:33:24 +0300

    destructuring lab test

---
+++ newFile.js
@@ -0,0 +1,8 @@
+// cow: "moo",
+// horse: "neigh",
+//sheep: "baa",
+// pig: "oink",
+//chicken: "cluck",
+//];
+const animalSounds = ["moo", "neigh", "baa", "oink", "cluck"];
+exports.animalSounds = animalSounds;
